611cb76172633c9142b7a6aab94a29052c07c4ec
Crowdbotics <team@crowdbotics.com>
2021-08-17 15:31:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royal_fog_29881Identifier",
+  url: "https://royal_fog_29881Identifier.botics.co",
+  api: "https://royal_fog_29881Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
